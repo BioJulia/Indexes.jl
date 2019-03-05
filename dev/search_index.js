70502f5fec143535db3a4812cc524b6e37826329
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-05 03:13:12 +0000

    build based on 87271e9

---
+++ dev/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Indexes.jl-1",
+    "page": "Home",
+    "title": "Indexes.jl",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "You can install Indexes from the Julia REPL:julia> Pkg.add(\"Indexes\")If you are interested in the cutting edge of the development, please check out the master branch to try new features before release."
+},
+
+]}
