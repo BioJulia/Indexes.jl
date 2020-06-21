7283c3af5c9b1d0eead5f2c8b557b73a03b7d161
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2020-06-21 21:48:18 +1000

    Polyfil for bytesavailable

--- src/Indexes.jl
+++ src/Indexes.jl
@@ -14,6 +14,18 @@ import BGZFStreams
 import BioGenerics
 import GenomicFeatures: Interval
 
+function Base.bytesavailable(stream::BGZFStreams.BGZFStream{IOStream})
+
+    block_index = BGZFStreams.ensure_buffered_data(stream)
+    if block_index == 0
+        return 0
+    end
+    block = stream.blocks[block_index]
+
+    return length(block.position:block.size)
+
+end
+
 include("chunk.jl")
 include("bgzfindex.jl")
 include("tabix.jl")
