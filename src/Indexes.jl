4883728c0c3d5aac432bbc8f2df12cdcfc7ee458
Ciarán O’Mara <Ciaran.OMara@utas.edu.au>
2021-03-31 08:56:13 +1100

    Amendments
    Base.bytesavailable is provided by CodecBGZF.
    Add compat entry for CodecBGZF.
    Keep using TranscodingStreams.

--- src/Indexes.jl
+++ src/Indexes.jl
@@ -8,21 +8,11 @@
 
 module Indexes
 
+using TranscodingStreams
+
 import CodecBGZF
-import BioCore
+import BioGenerics
 import GenomicFeatures: Interval
-
-function Base.bytesavailable(stream::BGZFStreams.BGZFStream{IOStream})
-
-    block_index = BGZFStreams.ensure_buffered_data(stream)
-    if block_index == 0
-        return 0
-    end
-    block = stream.blocks[block_index]
-
-    return length(block.position:block.size)
-
-end
 
 include("chunk.jl")
 include("bgzfindex.jl")
