8abe585299ca97d1a180824b2d8432cb268e8c75
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2021-11-27 22:44:34 +1100

    Include documenter tests in unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,15 @@
 using Test
+using Documenter
+
 using Indexes
 
 @testset "Indexes" begin
     # TODO
     # @test GenomicFeatures.Indexes.Tabix === GenomicFeatures.Indexes.Tabix
+
+
+# Include doctests.
+DocMeta.setdocmeta!(Indexes, :DocTestSetup, :(using Indexes); recursive=true)
+doctest(Indexes; manual = false)
+
 end
