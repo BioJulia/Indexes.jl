2309b9fefb0de102a79519328d0138ce82367e32
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2021-11-27 22:44:34 +1100

    Include documenter tests in unit tests

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,21 @@
 using Pkg
 using Documenter, Indexes
 
+DocMeta.setdocmeta!(Indexes, :DocTestSetup, :(using Indexes); recursive=true)
+
 makedocs(
     format = Documenter.HTML(
         edit_link = "develop"
     ),
     modules = [Indexes],
+    checkdocs = :all,
+    linkcheck = true,
     sitename = "Indexes.jl",
+    authors = replace(join(Pkg.TOML.parsefile("Project.toml")["authors"], ", "), r" <.*?>" => "" ) * ", The BioJulia Organisation, and other contributors.",
     pages = [
         "Home" => "index.md",
         "API Reference" => "man/api.md"
     ],
-    authors = replace(join(Pkg.TOML.parsefile("Project.toml")["authors"], ", "), r" <.*?>" => "" ) * ", The BioJulia Organisation, and other contributors."
 )
 deploydocs(
     repo = "github.com/BioJulia/Indexes.jl.git",
